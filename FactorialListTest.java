5e4536ee497cdadd004172f278bcf4633f806394
Somyadeep-Chowdhury <45650019+Somyadeep-Chowdhury@users.noreply.github.com>
2018-12-21 10:24:14 +0530

    Question7 testcases

---
+++ FactorialListTest.java
@@ -0,0 +1,59 @@
+package com.stackroute;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FactorialListTest {
+
+    @Test
+    public void test1
+
+    {
+        FactorialList ob1 = new FactorialList();
+        assertEquals(1,ob1.factorial(1));
+    }
+
+    @Test
+    public void test2
+
+    {
+        FactorialList ob2 = new FactorialList();
+        assertEquals(2,ob2.factorial(2));
+    }
+
+    @Test
+    public void test3
+
+    {
+        FactorialList ob3 = new FactorialList();
+        assertEquals(120,ob3.factorial(5));
+    }
+
+    @Test
+    public void test4
+
+    {
+        FactorialList ob4 = new FactorialList();
+        assertEquals("out of range",ob4.factorial(14));
+    }
+
+
+    @Test
+    public void test5
+
+    {
+        FactorialList ob5 = new FactorialList();
+        assertEquals("out of range",ob5.factorial(13);
+    }
+
+    @Test
+    public void test6
+
+    {
+        FactorialList ob6 = new FactorialList();
+        assertEquals(479001600,ob6.factorial(12));
+    }
+
+
+}
