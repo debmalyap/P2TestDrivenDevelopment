15edd2279f38ebdd7045c47b609c31bdb4047205
Somyadeep-Chowdhury <45650019+Somyadeep-Chowdhury@users.noreply.github.com>
2018-12-21 11:08:14 +0530

    final commit Q7

--- src/test/java/com/stackroute/FactorialListTest.java
+++ src/test/java/com/stackroute/FactorialListTest.java
@@ -1,67 +1,53 @@
 package com.stackroute;
 
 import org.junit.Test;
-
+// when the input is more 12 it will return -1
 import static org.junit.Assert.*;
 
 public class FactorialListTest {
 
     @Test
-    public void test1
+    public void test1()
 
     {
-        FactorialList ob1 = new FactorialList();
-        assertEquals(1,ob1.factorial(1));
+        assertEquals(1,FactorialList.factorial(1));
     }
 
     @Test
-    public void test2
+    public void test2()
 
     {
-        FactorialList ob2 = new FactorialList();
-        assertEquals(2,ob2.factorial(2));
+
+        assertEquals(2,FactorialList.factorial(2));
     }
 
     @Test
-    public void test3
+    public void test3()
 
     {
-        FactorialList ob3 = new FactorialList();
-        assertEquals(120,ob3.factorial(5));
+        assertEquals(120,FactorialList.factorial(5));
     }
 
     @Test
-    public void test4
+    public void test4()
 
     {
-        FactorialList ob4 = new FactorialList();
-        assertEquals("out of range",ob4.factorial(14));
+        assertEquals(-1,FactorialList.factorial(14));
     }
 
 
     @Test
-    public void test5
+    public void test5()
 
     {
-        FactorialList ob5 = new FactorialList();
-        assertFalse("out of range",ob5.factorial(13);
+        assertEquals(-1,FactorialList.factorial(13));
     }
 
     @Test
-    public void test6
+    public void test6()
 
     {
-        FactorialList ob6 = new FactorialList();
-        assertEquals(479001600,ob6.factorial(12));
+        assertEquals(479001600,FactorialList.factorial(12));
     }
-
-    @Test
-    public void test7
-
-    {
-        FactorialList ob7 = new FactorialList();
-        assertNull("Enter some value",);
-    }
-
 
 }
